40bde75b74472cdbb908f11a99838a6d93a00468
Luciano <cybergenik@gmail.com>
2023-04-02 01:00:31 -0600

    init rudex

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use std::fs;
+use std::env;
+use std::process;
+use std::thread;
+use std::sync::mpsc::{Sender, channel};
+use std::path::PathBuf;
+
+fn usage() {
+    println!("
+Rudex (Rust-indexer): Simple multi-threaded Filesystem traverser
+USAGE: 
+    rudex <path to dir/file>
+    Ex: rudex /home/user1
+        rudex /home/user1 /home/user2
+OPTIONS:
+    --help: Prints this message")
+}
+
+fn traverse(file: &PathBuf, ch: Sender<u64>) {
+    let dirs = fs::read_dir(file).unwrap_or_else(|err| {
+        eprintln!("Rudex: \"{:}\" {}", file.to_str().unwrap(), err);
+        process::exit(1);
+    });
+    let mut total:u64 = 0;
+    let mut threads:u8 = 0;
+    let (tx, rx) = channel();
+    for entry in dirs.into_iter() {
+        let entry = entry.unwrap();
+        if entry.path().is_dir() {
+            threads += 1;
+            let tnx = tx.clone();
+            thread::spawn(move|| {
+                traverse(&entry.path(), tnx);
+            });
+        } else {
+            total += entry.metadata().unwrap().len();
+        }
+    }
+    for _ in 0..threads {
+        total += rx.recv().unwrap();
+    }
+    ch.send(total).unwrap_or_else(|err| {
+        eprintln!("Rudex: Channel error {}", err);
+        process::exit(1);
+    });
+}
+
+const KILOBYTE:u64 = 1024;
+const MEGABYTE:u64 = 1_048_576;
+const GIGABYTE:u64 = 1_073_741_824;
+
+fn main() {
+    let args:Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Rudex: No args provided, for usage do: --help");
+        process::exit(1);
+    }
+    if args[1] == "--help" || args[1] == "-help" {
+        usage();
+        process::exit(0);
+    }
+    for fname in args[1..].iter() {
+        let metadata = fs::metadata(PathBuf::from(fname)).unwrap_or_else(|err| {
+            eprintln!("Rudex: \"{}\" {}", fname, err);
+            process::exit(1);
+        });
+
+        let total_size:u64;
+        if metadata.is_dir() {
+            let (tx, rx) = channel();
+            let fname = fname.clone();
+            thread::spawn(move|| {
+                traverse(&fname.into(), tx);
+            });
+            total_size = rx.recv().unwrap();
+        } else {
+            total_size = metadata.len()
+        }
+
+        if total_size < KILOBYTE {
+            println!("{} size: {:}", fname, total_size); 
+        } else if total_size < MEGABYTE {
+            println!("{} size: {:.1}K", fname, total_size as f64/KILOBYTE as f64); 
+        } else if total_size < GIGABYTE {
+            println!("{} size: {:.1}Mb", fname, total_size as f64/MEGABYTE as f64); 
+        } else {
+            println!("{} size: {:.1}Gb", fname, total_size as f64/GIGABYTE as f64); 
+        }
+    }
+}
