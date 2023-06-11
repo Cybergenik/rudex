a9981afb612b99d77491d1a64174517f1fe760e5
cybergenik <cybergenik@gmail.com>
2023-06-11 13:01:50 -0500

    fixing, kind of

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,57 @@
-use std::io;
-use std::fs;
 use std::env;
-use std::process;
+use std::fs;
+use std::future::Future;
+use std::io;
 use std::path::PathBuf;
+use std::pin::Pin;
+use std::process;
 
 use tokio::task::JoinSet;
 
 fn usage() {
-    println!("
+    println!(
+        "
 Rudex (Rust-indexer): Simple multi-threaded Filesystem traverser
 USAGE: 
     rudex <path to dir/file>
     Ex: rudex /home/user1
         rudex /home/user1 /home/user2
 OPTIONS:
-    --help: Prints this message")
+    --help: Prints this message"
+    )
 }
 
-async fn traverse(file: &PathBuf) -> io::Result<u64> {
-    let mut total:u64 = 0;
-    let mut tasks = JoinSet::new();
-    for entry in fs::read_dir(file)? {
-        let entry = entry.unwrap();
-        if entry.path().is_dir() {
-            tasks.spawn(async move {
-                return traverse(&entry.path()).await.unwrap();
-            });
-        } else {
-            total += entry.metadata().unwrap().len();
+fn traverse(file: &PathBuf) -> Pin<Box<dyn Future<Output = u64>>> {
+    let fut = async move {
+        let mut total: u64 = 0;
+        let mut tasks = JoinSet::new();
+        for entry in fs::read_dir(file)? {
+            let entry = entry.unwrap();
+            if entry.path().is_dir() {
+                tasks.spawn(async move {
+                    return traverse(&entry.path()).await;
+                });
+            } else {
+                total += entry.metadata().unwrap().len();
+            }
         }
-    }
-    tasks.detach_all();
-    while let Some(res) = tasks.join_next().await {
-        let sub_total:u64 = res.unwrap();
-        total += sub_total;
-    }
-    Ok(total)
+        tasks.detach_all();
+        while let Some(res) = tasks.join_next().await {
+            let sub_total: u64 = res.unwrap();
+            total += sub_total;
+        }
+        Ok(total)
+    };
+    Box::pin(fut);
 }
 
-const KILOBYTE:u64 = 1024;
-const MEGABYTE:u64 = 1_048_576;
-const GIGABYTE:u64 = 1_073_741_824;
+const KILOBYTE: u64 = 1024;
+const MEGABYTE: u64 = 1_048_576;
+const GIGABYTE: u64 = 1_073_741_824;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let args:Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         eprintln!("Rudex: No args provided, for usage do: --help");
         process::exit(1);
@@ -54,27 +61,39 @@ async fn main() -> io::Result<()> {
         process::exit(0);
     }
     for fname in args[1..].iter() {
-        let metadata = fs::metadata(PathBuf::from(fname)).unwrap_or_else(|err| {
+        let metadata = fs::metadata(fname).unwrap_or_else(|err| {
             eprintln!("Rudex: \"{}\" {}", fname, err);
             process::exit(1);
         });
 
-        let total_size:u64;
+        let total_size: u64;
         if metadata.is_dir() {
             let fname = fname.clone();
-            total_size = traverse(&fname.into()).await.unwrap();
+            total_size = traverse(&fname.into()).await;
         } else {
             total_size = metadata.len()
         }
 
         if total_size < KILOBYTE {
-            println!("{} size: {:}", fname, total_size); 
+            println!("{} size: {:}", fname, total_size);
         } else if total_size < MEGABYTE {
-            println!("{} size: {:.1}K", fname, total_size as f64/KILOBYTE as f64); 
+            println!(
+                "{} size: {:.1}K",
+                fname,
+                total_size as f64 / KILOBYTE as f64
+            );
         } else if total_size < GIGABYTE {
-            println!("{} size: {:.1}Mb", fname, total_size as f64/MEGABYTE as f64); 
+            println!(
+                "{} size: {:.1}Mb",
+                fname,
+                total_size as f64 / MEGABYTE as f64
+            );
         } else {
-            println!("{} size: {:.1}Gb", fname, total_size as f64/GIGABYTE as f64); 
+            println!(
+                "{} size: {:.1}Gb",
+                fname,
+                total_size as f64 / GIGABYTE as f64
+            );
         }
     }
     Ok(())
